c827b792b24e4b4d00f7b1872781a8e3250448d8
Gordon <liyang_xx@icloud.com>
2017-08-19 15:59:33 +0800

    。。。

---
+++ takeEasy/takeEasy/Classes/轻松一刻/ViewModel/FuntimeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FuntimeViewModel.swift
+//  takeEasy
+//
+//  Created by Gordon on 2017/8/19.
+//  Copyright © 2017年 Gordon. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class FuntimeViewModel: NSObject {
+
+}
